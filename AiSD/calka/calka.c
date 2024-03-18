1ca9975d26054cfb2f71326a4c58f78867829d45
Remxin <remxin7@gmail.com>
2024-03-18 15:10:36 +0100

    AiSD projekt 1

---
+++ AiSD/calka/calka.c
@@ -0,0 +1,54 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+extern void initRandom();
+extern double f3(double x);
+extern double f4(double x);
+extern double prostokaty(double( *f)(double));
+extern double trapezy(double (*f)(double));
+extern double mc(double(*f)(double));
+
+
+double c_od, c_do;
+
+
+
+double f1(double x)
+{
+    return -3.13*x*x*x*x+14.5*x*x-3;
+}
+double f2(double x)
+{
+    double y;
+    y=cos(2*x*x)*2*sin(8*x)-cos(5*x)+1;
+    return y;
+}
+
+int main(int argc, char *argv[])
+{
+    initRandom();
+    printf("od=");
+    scanf("%lf",&c_od);
+    printf("do=");
+    scanf("%lf",&c_do);
+    printf ("\n\nFunkcja 1 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f1));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f1));
+     printf( "wynik MC   - %lf\n", mc(f1));
+    printf("\n\nFunkcja 2 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f2));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f2));
+     printf("wynik MC   - %lf\n", mc(f2));
+    printf ("\n\nFunkcja 3 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f3));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f3));
+     printf( "wynik MC   - %lf\n", mc(f3));
+    printf("\n\nFunkcja 4 \n");
+    printf( "wynik prostokaty - %lf\n" ,prostokaty(f4));
+    printf( "wynik trapezy    - %lf\n" ,trapezy(f4));
+     printf( "wynik MC   - %lf\n", mc(f4));
+
+    return 0;
+}
---
+++ AiSD/calka/calki.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <float.h>
+
+#define lp 10000000
+
+double c_od,c_do;
+
+
+void initRandom() {
+    srand(time(NULL));
+}
+
+double getRandomNum(double min, double max) {
+    return min + ((double)rand() / RAND_MAX) * (max - min);
+}
+
+void assingMinMaxForRange(double *min, double *max, double( *f)(double)) {
+    double m = DBL_MAX;
+    double M = -DBL_MAX;
+
+
+    double krok=(c_do-c_od)/lp;
+    double current;
+
+    for (double x = c_od; x <= c_do; x += krok) {
+        current = (*f)(x);
+        if (current > M) {
+            M = current;
+        }
+        if (current < m) {
+            m = current;
+        }
+    }
+    *min = m;
+    *max = M;
+}
+
+
+
+double f3(double x)
+{
+    return atan(x) / pow(1 + x*x, 3./2);
+}
+double f4(double x) {
+
+    return pow(x, 2)*sin(x);
+}
+
+
+double prostokaty(double( *f)(double))
+{
+    double krok, prev, current, suma=0;
+    krok=(c_do-c_od)/lp;
+
+    prev = (*f)(c_od);
+    for (double x = c_od; x <= c_do; x += krok) {
+        current = (*f)(x);
+        suma += (prev + current);
+        prev = current;
+    }
+    return (suma)/2*krok;
+}
+
+double trapezy(double (*f)(double)) {
+    double krok = (c_do-c_od)/lp;
+    double calka = ((*f)(c_od)+(*f)(c_do))/2;
+    for (double x = c_od + krok; x < c_do; x += krok) {
+        calka += (*f)(x);
+    }
+    return calka * krok;
+}
+
+
+
+double mc(double(*f)(double))
+{
+    double minV, maxV;
+    double px, py, pxVal;
+    int lpkt = 0;
+
+    assingMinMaxForRange(&minV, &maxV, *f);
+
+    for (int i = 0; i < lp; i++) {
+         px = getRandomNum(c_od, c_do);
+         py = getRandomNum(minV, maxV);
+         pxVal = (*f)(px);
+
+         if (py >= 0 && py <= pxVal) lpkt++;
+         else if (py < 0 && py >= pxVal) lpkt--;
+    }
+
+    double rectF = (c_do - c_od) * (maxV - minV);
+
+    return lpkt * rectF / lp;
+
+
+}
+
